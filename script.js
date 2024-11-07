ea92590ff91dcb4612155fb85e7471f9ddcc806c
VLDVNux <faldifian2@gmail.com>
2024-11-07 17:20:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let x = "Econature";
+// let y = "2024";
+
+// alert(x+y);
+// console.log(x+y); 
+
+// let x = ["Sampah plastik sekali pakai","Daur Ulang"];
+// x[2]="Penanaman Pohon";
+// console.log(x);
+
+// let x = {firsname:'Valdivian', lastname:'Aprilisianto'};
+// console.log(x);
+
+ function addname(firsname, lastname) {
+    console.log(firsname, lastname)
+ }
+
+ addname('Valdivian Aprilisianto','Wahid Nurrohman')
